1cdc2ffde84a8e470974242c0dcddd0bd61e4754
Nikola Maroski <nikolamaroski@hotmail.com>
2018-12-20 17:15:21 +0100

    Movement News

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PoslediciComponent } from './posledici/posledici.component';
-import { SoluciiComponent } from './solucii/solucii.component';
 import { WakeUpCallComponent } from './wake-up-call/wake-up-call.component';
 import { MovementComponent } from './movement/movement.component';
 import { HomeComponent } from './home/home.component';
@@ -13,8 +11,6 @@ import { NewsComponent } from './news/news.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PoslediciComponent,
-    SoluciiComponent,
     WakeUpCallComponent,
     MovementComponent,
     HomeComponent,
